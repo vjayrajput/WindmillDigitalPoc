f25a823309d0da5dc092f8a976f2fe75bf9fe5a2
Rajput Vijay <vjayrajput@gmail.com>
2024-10-02 00:30:18 +0530

    - created crypto detail page UI and navigation

--- lib/core/resources/strings.dart
+++ lib/core/resources/strings.dart
@@ -21,10 +21,20 @@ class Strings {
       "Request to API was cancelled.";
   static const String connectionFailedCheckYourConnection =
       "Connection failed. Check your internet connection.";
-  static const String failedToLoadCryptocurrencies = "Failed to load cryptocurrencies with status:";
+  static const String failedToLoadCryptocurrencies =
+      "Failed to load cryptocurrencies with status:";
 
   static const String routeHome = "/";
+  static const String routeDetails = "/details";
   static const String appTitle = "Cryptocurrencies";
   static const String currentCryptocurrencies = "Current Cryptocurrencies";
   static const String favorites = "Favorites";
+  static const String noData = "No data";
+
+  static const String cryptocurrencyId = "Cryptocurrency ID : ";
+  static const String name = "Name : ";
+  static const String symbol = "Symbol : ";
+  static const String rank = "Rank : ";
+  static const String firstHistoricalData = "First Historical Data : ";
+  static const String lastHistoricalData = "Last Historical Data : ";
 }
