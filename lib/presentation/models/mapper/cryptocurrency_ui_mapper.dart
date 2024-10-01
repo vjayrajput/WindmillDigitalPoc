100c0323fe7e1bfb3dc0cb6025793bc0d3d20b2f
Rajput Vijay <vjayrajput@gmail.com>
2024-10-01 21:38:45 +0530

    - created ui models and its mapper

---
+++ lib/presentation/models/mapper/cryptocurrency_ui_mapper.dart
@@ -0,0 +1,20 @@
+import 'package:windmill_digital_poc/domain/entities/cryptocurrency_entity.dart';
+import 'package:windmill_digital_poc/presentation/models/cryptocurrency_ui_model.dart';
+import 'package:windmill_digital_poc/presentation/models/mapper/platform_ui_mapper.dart';
+
+class CryptocurrencyUiMapper {
+  static CryptocurrencyUiModel toUiModel(CryptocurrencyEntity entity) {
+    return CryptocurrencyUiModel(
+      id: entity.id,
+      name: entity.name,
+      symbol: entity.symbol,
+      slug: entity.slug,
+      rank: entity.rank,
+      firstHistoricalData: entity.firstHistoricalData ?? "",
+      lastHistoricalData: entity.lastHistoricalData ?? "",
+      platform: entity.platform != null
+          ? PlatformUiMapper.toUiModel(entity.platform!)
+          : null, // Handle null platform properly
+    );
+  }
+}
