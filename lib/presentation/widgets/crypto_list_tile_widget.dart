32853ad930cd645495acf29e1c0958a24eda940b
Rajput Vijay <vjayrajput@gmail.com>
2024-10-01 23:42:12 +0530

    - created crypto item widget

---
+++ lib/presentation/widgets/crypto_list_tile_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:windmill_digital_poc/core/resources/styles.dart';
+import 'package:windmill_digital_poc/presentation/models/cryptocurrency_ui_model.dart';
+
+class CryptoListTile extends StatelessWidget {
+  final CryptocurrencyUiModel crypto;
+
+  const CryptoListTile({super.key, required this.crypto});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: AppThemes.cardPadding,
+      child: Card(
+        elevation: AppThemes.cardElevation,
+        child: ListTile(
+          title: Text(
+            crypto.name,
+            style: AppTextStyles.titleStyle,
+          ),
+          subtitle: Text(
+            "(${crypto.symbol})",
+            style: AppTextStyles.subtitleStyle,
+          ),
+          onTap: () {},
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/widgets/crypto_list_view_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:windmill_digital_poc/presentation/models/cryptocurrency_ui_model.dart';
+import 'package:windmill_digital_poc/presentation/widgets/crypto_list_tile_widget.dart';
+
+class CryptoListView extends StatelessWidget {
+  final List<CryptocurrencyUiModel> cryptocurrencies;
+  final ScrollController scrollController;
+
+  const CryptoListView({
+    super.key,
+    required this.cryptocurrencies,
+    required this.scrollController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      controller: scrollController,
+      itemCount: cryptocurrencies.length,
+      itemBuilder: (context, index) {
+        final crypto = cryptocurrencies[index];
+        return CryptoListTile(crypto: crypto);
+      },
+    );
+  }
+}
