62ff10e33dce04e5b84b98039d7701ce0801b1c9
Rajput Vijay <vjayrajput@gmail.com>
2024-10-02 00:03:04 +0530

    - fetched crypto currencies data and display in UI

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:windmill_digital_poc/core/resources/strings.dart';
+import 'package:windmill_digital_poc/presentation/pages/cryptocurrencies_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(Strings.appTitle),
-      ),
-      body: Center(
-        child: Text(Strings.appTitle),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(Strings.appTitle),
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                text: Strings.currentCryptocurrencies,
+                icon: Icon(Icons.currency_bitcoin_outlined),
+              ),
+              Tab(
+                text: Strings.favorites,
+                icon: Icon(Icons.favorite_border_outlined),
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            CryptocurrenciesPage(),
+            CryptocurrenciesPage(),
+          ],
+        ),
       ),
     );
   }
