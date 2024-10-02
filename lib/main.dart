02e12e806b53083b65daf401cb9d64c0c3218436
Rajput Vijay <vjayrajput@gmail.com>
2024-10-02 18:11:33 +0530

    - created favorites bloc

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,23 @@ import 'package:windmill_digital_poc/core/resources/strings.dart';
 import 'package:windmill_digital_poc/core/resources/styles.dart';
 import 'package:windmill_digital_poc/di/di.dart';
 import 'package:windmill_digital_poc/presentation/bloc/cryptocurrency_bloc.dart';
+import 'package:windmill_digital_poc/presentation/bloc/favorite_currency_bloc.dart';
 import 'package:windmill_digital_poc/presentation/pages/cryptocurrency_detail_page.dart';
 import 'package:windmill_digital_poc/presentation/pages/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   setupDependencies();
   runApp(
-    BlocProvider(
-      create: (context) => getIt<CryptocurrencyBloc>(),
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => getIt<CryptocurrencyBloc>(),
+        ),
+        BlocProvider(
+          create: (context) => getIt<FavoriteCurrencyBloc>(),
+        ),
+      ],
       child: const MyApp(),
     ),
   );
