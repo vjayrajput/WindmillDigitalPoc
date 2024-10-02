e81d419561c0ba90bf43a92b289511db4c3619ae
Rajput Vijay <vjayrajput@gmail.com>
2024-10-02 16:10:41 +0530

    - updated models and domain for hive

--- lib/data/models/cryptocurrency_model.dart
+++ lib/data/models/cryptocurrency_model.dart
@@ -1,17 +1,37 @@
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:windmill_digital_poc/data/models/platform_model.dart';
 
 part 'cryptocurrency_model.g.dart';
 
+@HiveType(typeId: 0)
 @JsonSerializable()
 class CryptocurrencyModel {
+  @HiveField(0)
   final int id;
+
+  @HiveField(1)
   final int rank;
+
+  @HiveField(2)
   final String name;
+
+  @HiveField(3)
   final String symbol;
+
+  @HiveField(4)
   final String slug;
+
+  @HiveField(5)
+  final int isActive;
+
+  @HiveField(6)
   final String? firstHistoricalData;
+
+  @HiveField(7)
   final String? lastHistoricalData;
+
+  @HiveField(8)
   final PlatformModel? platform;
 
   CryptocurrencyModel({
@@ -20,6 +40,7 @@ class CryptocurrencyModel {
     required this.name,
     required this.symbol,
     required this.slug,
+    required this.isActive,
     required this.firstHistoricalData,
     required this.lastHistoricalData,
     this.platform,
--- lib/data/models/platform_model.dart
+++ lib/data/models/platform_model.dart
@@ -1,19 +1,32 @@
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'platform_model.g.dart';
 
+@HiveType(typeId: 1)
 @JsonSerializable()
 class PlatformModel {
+  @HiveField(0)
   final int id;
+
+  @HiveField(1)
   final String name;
+
+  @HiveField(2)
   final String symbol;
+
+  @HiveField(3)
   final String slug;
+
+  @HiveField(4)
+  final String tokenAddress;
 
   PlatformModel({
     required this.id,
     required this.name,
     required this.symbol,
     required this.slug,
+    required this.tokenAddress,
   });
 
   factory PlatformModel.fromJson(Map<String, dynamic> json) =>
