0f4622acdf83ffa129273e84039434e49bbda406
Rajput Vijay <vjayrajput@gmail.com>
2024-10-02 16:12:17 +0530

    - updated generated files of platforms due to hive

--- lib/data/models/platform_model.g.dart
+++ lib/data/models/platform_model.g.dart
@@ -3,15 +3,66 @@
 part of 'platform_model.dart';
 
 // **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class PlatformModelAdapter extends TypeAdapter<PlatformModel> {
+  @override
+  final int typeId = 1;
+
+  @override
+  PlatformModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return PlatformModel(
+      id: fields[0] as int,
+      name: fields[1] as String,
+      symbol: fields[2] as String,
+      slug: fields[3] as String,
+      tokenAddress: fields[4] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, PlatformModel obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.symbol)
+      ..writeByte(3)
+      ..write(obj.slug)
+      ..writeByte(4)
+      ..write(obj.tokenAddress);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is PlatformModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+// **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
 PlatformModel _$PlatformModelFromJson(Map<String, dynamic> json) =>
     PlatformModel(
-      id: (json['id'] as num).toInt(),
-      name: json['name'] as String,
-      symbol: json['symbol'] as String,
-      slug: json['slug'] as String,
+      id: (json['id'] ?? 0) as int,
+      name: (json['name'] ?? "") as String,
+      symbol: (json['symbol'] ?? "") as String,
+      slug: (json['slug'] ?? "") as String,
+      tokenAddress: (json['tokenAddress'] ?? "") as String,
     );
 
 Map<String, dynamic> _$PlatformModelToJson(PlatformModel instance) =>
@@ -20,4 +71,5 @@ Map<String, dynamic> _$PlatformModelToJson(PlatformModel instance) =>
       'name': instance.name,
       'symbol': instance.symbol,
       'slug': instance.slug,
+      'tokenAddress': instance.tokenAddress,
     };
