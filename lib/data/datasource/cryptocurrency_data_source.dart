c373c57ff15cc98a59f7cf973e9aeb17e6892a71
Rajput Vijay <vjayrajput@gmail.com>
2024-10-03 00:45:18 +0530

    - used data source impl

--- lib/data/datasource/cryptocurrency_data_source.dart
+++ lib/data/datasource/cryptocurrency_data_source.dart
@@ -1,15 +1,6 @@
 import 'package:windmill_digital_poc/data/models/cryptocurrency_model.dart';
-import 'package:windmill_digital_poc/data/service/api_service.dart';
 
-class CryptocurrencyDataSource {
-  final ApiService apiService;
-
-  CryptocurrencyDataSource(this.apiService);
-
-  Future<List<CryptocurrencyModel>> getCryptocurrencies(
-      {required int start, required int limit}) async {
-    final List<CryptocurrencyModel> cryptocurrencies =
-        await apiService.fetchCryptocurrencies(start: start, limit: limit);
-    return cryptocurrencies;
-  }
+abstract class CryptocurrencyDataSource {
+  Future<List<CryptocurrencyModel>> fetchCryptocurrencies(
+      {required int start, required int limit});
 }
