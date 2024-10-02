9431692ca5a38ce7219e0c39706a923f33fab40d
Rajput Vijay <vjayrajput@gmail.com>
2024-10-02 18:42:43 +0530

    - created favorites api implemented

---
+++ lib/presentation/widgets/cryptocurrency_details_card_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:windmill_digital_poc/core/resources/strings.dart';
+import 'package:windmill_digital_poc/core/resources/styles.dart';
+import 'package:windmill_digital_poc/presentation/models/cryptocurrency_ui_model.dart';
+
+class CryptocurrencyDetailsCard extends StatelessWidget {
+  final CryptocurrencyUiModel cryptocurrency;
+
+  const CryptocurrencyDetailsCard({super.key, required this.cryptocurrency});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: AppThemes.cardElevation,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              "${Strings.cryptocurrencyId}${cryptocurrency.id}",
+              style: AppTextStyles.titleStyle,
+            ),
+            Text(
+              "${Strings.name}${cryptocurrency.name}",
+              style: AppTextStyles.titleStyle,
+            ),
+            Text(
+              "${Strings.symbol}${cryptocurrency.symbol}",
+              style: AppTextStyles.titleStyle,
+            ),
+            Text(
+              "${Strings.rank}${cryptocurrency.rank}",
+              style: AppTextStyles.titleStyle,
+            ),
+            Text(
+              "${Strings.firstHistoricalData}${cryptocurrency.firstHistoricalData}",
+              style: AppTextStyles.titleStyle,
+            ),
+            Text(
+              "${Strings.lastHistoricalData}${cryptocurrency.lastHistoricalData}",
+              style: AppTextStyles.titleStyle,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
