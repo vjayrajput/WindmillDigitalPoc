7712a8829f583d1abdbe134dc82a15f772d2a4dc
Rajput Vijay <vjayrajput@gmail.com>
2024-10-03 02:06:56 +0530

    - injected same model in 2 hive box with injection

--- lib/data/datasource/favorite_data_source_Impl.dart
+++ lib/data/datasource/favorite_data_source_Impl.dart
@@ -3,27 +3,27 @@ import 'package:windmill_digital_poc/data/datasource/favorite_data_source.dart';
 import 'package:windmill_digital_poc/data/models/cryptocurrency_model.dart';
 
 class FavoriteDataSourceImpl implements FavoriteDataSource {
-  final Box<CryptocurrencyModel> hiveBox;
+  final Box<CryptocurrencyModel> favoriteBox;
 
-  FavoriteDataSourceImpl(this.hiveBox);
+  FavoriteDataSourceImpl({required this.favoriteBox});
 
   @override
   Future<List<CryptocurrencyModel>> getFavorites() async {
-    return hiveBox.values.toList();
+    return favoriteBox.values.toList();
   }
 
   @override
   Future<void> saveFavorite(CryptocurrencyModel currency) async {
-    await hiveBox.put(currency.id, currency);
+    await favoriteBox.put(currency.id, currency);
   }
 
   @override
   Future<void> deleteFavorite(int currencyId) async {
-    await hiveBox.delete(currencyId);
+    await favoriteBox.delete(currencyId);
   }
 
   @override
   Future<bool> isFavorite(int currencyId) async {
-    return hiveBox.containsKey(currencyId);
+    return favoriteBox.containsKey(currencyId);
   }
 }
--- lib/data/service/api_service.dart
+++ lib/data/service/api_service.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:windmill_digital_poc/core/errors/error_handler.dart';
 import 'package:windmill_digital_poc/core/resources/strings.dart';
 import 'package:windmill_digital_poc/data/models/cryptocurrency_model.dart';
@@ -6,9 +7,13 @@ import 'package:windmill_digital_poc/data/models/cryptocurrency_model.dart';
 class ApiService {
   final Dio _dio = Dio();
 
+  final Box<CryptocurrencyModel> cryptocurrencyBox;
+
   static const String apiUrl =
       'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map';
   static const String apiKey = '2592e201-7cb0-41b4-81d5-abacc60ac4ee';
+
+  ApiService({required this.cryptocurrencyBox});
 
   Future<List<CryptocurrencyModel>> fetchCryptocurrencies(
       {required int start, required int limit}) async {
@@ -25,9 +30,14 @@ class ApiService {
       print("apiService response : ${response}");
       if (response.statusCode == 200) {
         final List<dynamic> data = response.data['data'];
-        return data
-            .map((crypto) => CryptocurrencyModel.fromJson(crypto))
-            .toList();
+
+        // Save to Hive
+        List<CryptocurrencyModel> cryptocurrencies =
+            data.map((crypto) => CryptocurrencyModel.fromJson(crypto)).toList();
+        for (var crypto in cryptocurrencies) {
+          await cryptocurrencyBox.put(crypto.id, crypto); // Store by ID
+        }
+        return cryptocurrencies;
       } else {
         throw Exception(
             "${Strings.failedToLoadCryptocurrencies} ${response.statusCode}");
