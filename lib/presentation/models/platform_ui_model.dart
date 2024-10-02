4c718b4470725829626b6dd2fb9752ddd9c4dc6e
Rajput Vijay <vjayrajput@gmail.com>
2024-10-02 16:10:41 +0530

    - updated models and domain for hive

--- lib/presentation/models/platform_ui_model.dart
+++ lib/presentation/models/platform_ui_model.dart
@@ -5,14 +5,16 @@ class PlatformUiModel extends Equatable {
   final String name;
   final String symbol;
   final String slug;
+  final String tokenAddress;
 
-  PlatformUiModel({
+  const PlatformUiModel({
     required this.id,
     required this.name,
     required this.symbol,
     required this.slug,
+    required this.tokenAddress,
   });
 
   @override
-  List<Object> get props => [id, name, symbol, slug];
+  List<Object> get props => [id, name, symbol, slug, tokenAddress];
 }
