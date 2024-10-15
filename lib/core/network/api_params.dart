7149acd4edf04872ea90c900a2812360fadb924d
Rajput Vijay <vjayrajput@gmail.com>
2024-10-15 13:44:44 +0530

    - Updated logic of network connectivity - Created dio object using DI

---
+++ lib/core/network/api_params.dart
@@ -0,0 +1,8 @@
+class ApiParams {
+  ApiParams._();
+
+  static const String start = "start";
+  static const String limit= "limit";
+  static const String headerProApiKey= "X-CMC_PRO_API_KEY";
+
+}
