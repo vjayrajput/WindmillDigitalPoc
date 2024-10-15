8ddf6a5010f4f7acb333e29098e8f51eae8080f1
Rajput Vijay <vjayrajput@gmail.com>
2024-10-15 14:20:09 +0530

    - separated favorite bloc to manage and load favorite bloc

---
+++ lib/presentation/bloc/favorite/manage_favorite_state.dart
@@ -0,0 +1,49 @@
+import 'package:equatable/equatable.dart';
+import 'package:windmill_digital_poc/presentation/models/cryptocurrency_ui_model.dart';
+
+abstract class ManageFavoriteState extends Equatable {
+  const ManageFavoriteState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class ManageFavoriteInitial extends ManageFavoriteState {}
+
+class ManageFavoriteLoading extends ManageFavoriteState {}
+
+class LoadedFavoriteStatus extends ManageFavoriteState {
+  final CryptocurrencyUiModel currency;
+  final bool isFavorite;
+
+  const LoadedFavoriteStatus({
+    required this.currency,
+    this.isFavorite = false,
+  });
+
+  LoadedFavoriteStatus copyWith({
+    CryptocurrencyUiModel? currency,
+    bool? isFavorite = false,
+  }) {
+    return LoadedFavoriteStatus(
+      currency: currency ?? this.currency,
+      isFavorite: isFavorite ?? this.isFavorite,
+    );
+  }
+
+  @override
+  List<Object?> get props => [currency, isFavorite];
+}
+
+class CurrencyIsFavorite extends ManageFavoriteState {}
+
+class CurrencyIsNotFavorite extends ManageFavoriteState {}
+
+class ManageFavoriteError extends ManageFavoriteState {
+  final String message;
+
+  const ManageFavoriteError(this.message);
+
+  @override
+  List<Object?> get props => [message];
+}
